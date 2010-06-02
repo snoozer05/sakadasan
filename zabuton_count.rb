b5f1369eeddeee274e3da6122373781a91ef9816
irasally <sally0721+github@gmail.com>
2010-06-02 00:49:49 -0700

    修正 1） 自分自身の座布団操作を出来なくする（from_idとuseridが同じ場合は処理を抜ける） 2） 座布団を数えた結果マイナス値になる場合は0に補正する 3） すでに0枚の人の座布団をさらに減らそうとした場合のエラーメッセージを変える
    動作確認していないのでケアレスミスが心配でたまりません。
    帰宅したら動作確認しますが、一旦コミットします。

--- zabuton_count.rb
+++ zabuton_count.rb
@@ -4,13 +4,17 @@ class ZabutonCount
   
   #
   # 座布団処理をします。
-  # param:twitterの本文（status）と、発言者ID(from_id)
+  # param:twitterの本文（status）、発言者ID(from_id)
   # return:返信status
   #
   def get_zabuton(status, from_id)
-    result = check(status)
+    result = check(status,from_id)
     if !result.nil?
-      return "@#{from_id}さんの座布団で、@#{result[:user_id]}さんの座布団が、現在#{result[:count]}枚になりました！"
+      if result[:res] == true
+        return "@#{from_id}さんの座布団で、#{result[:user_id]}さんの座布団が、現在#{result[:count]}枚になりました！"
+      else
+        return "@#{from_id}さん！#{result[:user_id]}さんの座布団はもうゼロよ！"
+      end
     end
     return nil
   end
@@ -18,17 +22,23 @@ class ZabutonCount
   #
   # 座布団をあげたかどうかチェックします。
   # 座布団をあげている場合は、座布団計算をします。
-  # param:twitterの本文(status)
+  # param:twitterの本文(status)、発言者ID(from_id)
   # return {[座布団対象id],[座布団の数]}
   #
-  def check(status)
+  def check(status,from_id)
    param = status.scan(/^@(\w+)\s*(\+\++|--+)/)
    if param.size != 0
      if param[0].size == 2
        userid = param[0][0]
        countstr = param[0][1]
-       model = save_zabuton( userid, countstr)
-       return { :user_id => model.user_id, :count => model.count }
+       if userid != from_id
+         model = save_zabuton( userid, countstr)
+         if !model.nil?
+           return { :user_id => model.user_id, :count => model.count , :res => true}
+         else
+           return { :user_id => userid , :count => 0, :res => false }
+         end
+       end
      end  
    end
    return nil
@@ -40,12 +50,22 @@ class ZabutonCount
   #
   def save_zabuton(userid,countstr)
     model = CountTable.find_by_user_id(userid)
-    if model != nil
-      model.count += get_count(countstr)
+    count = get_count(countstr)
+    if !model.nil?
+      if model.count == 0 && count < 0
+        return nil
+      else
+        model.count += count 
+        model.count = 0 if model.count < 0
+      end
     else
-      model = CountTable.new
-      model.user_id = userid
-      model.count = get_count(countstr)
+      if  count < 0
+        return nil
+      else
+        model = CountTable.new
+        model.user_id = userid
+        model.count = count 
+      end
     end
     model.save!
     return model
