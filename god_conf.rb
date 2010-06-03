25b0955c9aeb9a606db461399d9fd4f14933a73d
Hajime WAKAHARA <hajime.wakahara@gmail.com>
2010-06-03 15:20:33 +0900

    godの設定ファイルにpidファイルの削除を追加してみるテスト

--- god_conf.rb
+++ god_conf.rb
@@ -4,6 +4,7 @@ app_root = "#{ENV["HOME"]}/sakadasan"
 God.watch do |w|
   w.name = "sakadasan/tuple_space.rb"
   w.start = "cd #{app_root}; ./tuple_space.rb"
+  w.behavior(:clean_pid_file)
   w.start_if do |start|
     start.condition(:process_running) do |c|
       c.interval = 3.seconds
@@ -15,6 +16,7 @@ end
 God.watch do |w|
   w.name = "sakadasan/pull.rb"
   w.start = "cd #{app_root}; ./pull.rb"
+  w.behavior(:clean_pid_file)
   w.start_if do |start|
     start.condition(:process_running) do |c|
       c.interval = 3.seconds
@@ -26,6 +28,7 @@ end
 God.watch do |w|
   w.name = "sakadasan/invent.rb"
   w.start = "cd #{app_root}; ./invent.rb"
+  w.behavior(:clean_pid_file)
   w.start_if do |start|
     start.condition(:process_running) do |c|
       c.interval = 3.seconds
@@ -37,6 +40,7 @@ end
 God.watch do |w|
   w.name = "sakadasan/read.rb"
   w.start = "cd #{app_root}; ./read.rb"
+  w.behavior(:clean_pid_file)
   w.start_if do |start|
     start.condition(:process_running) do |c|
       c.interval = 3.seconds
@@ -48,6 +52,7 @@ end
 God.watch do |w|
   w.name = "sakadasan/push_tweet.rb"
   w.start = "cd #{app_root}; ./push_tweet.rb"
+  w.behavior(:clean_pid_file)
   w.start_if do |start|
     start.condition(:process_running) do |c|
       c.interval = 3.seconds
@@ -59,6 +64,7 @@ end
 God.watch do |w|
   w.name = "sakadasan/tweet.rb"
   w.start = "cd #{app_root}; ./tweet.rb"
+  w.behavior(:clean_pid_file)
   w.start_if do |start|
     start.condition(:process_running) do |c|
       c.interval = 3.seconds
@@ -70,6 +76,7 @@ end
 God.watch do |w|
   w.name = "sakadasan/reply.rb"
   w.start = "cd #{app_root}; ./reply.rb"
+  w.behavior(:clean_pid_file)
   w.start_if do |start|
     start.condition(:process_running) do |c|
       c.interval = 3.seconds
@@ -81,6 +88,7 @@ end
 God.watch do |w|
   w.name = "sakadasan/update.rb"
   w.start = "cd #{app_root}; ./update.rb"
+  w.behavior(:clean_pid_file)
   w.start_if do |start|
     start.condition(:process_running) do |c|
       c.interval = 3.seconds
@@ -92,6 +100,7 @@ end
 God.watch do |w|
   w.name = "sakadasan/push_adjust.rb"
   w.start = "cd #{app_root}; ./push_adjust.rb"
+  w.behavior(:clean_pid_file)
   w.start_if do |start|
     start.condition(:process_running) do |c|
       c.interval = 3.seconds
@@ -103,6 +112,7 @@ end
 God.watch do |w|
   w.name = "sakadasan/adjust.rb"
   w.start = "cd #{app_root}; ./adjust.rb"
+  w.behavior(:clean_pid_file)
   w.start_if do |start|
     start.condition(:process_running) do |c|
       c.interval = 3.seconds
