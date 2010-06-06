13aee4079c042bba29f2215babc7ad0c468dd46c
irasally <sally0721+github@gmail.com>
2010-06-06 18:40:42 +0900

    ・先頭以外に@hogehoge+++があっても読み込む ・返し文言を変更

--- zabuton_count.rb
+++ zabuton_count.rb
@@ -11,7 +11,7 @@ class ZabutonCount
     result = check(status,from_id)
     if !result.nil?
       if result[:res] == true
-        return "@#{from_id}さんの座布団で、#{result[:user_id]}さんの座布団が、現在#{result[:count]}枚になりました！"
+        return "@#{from_id}さんが#{result[:this_count].to_i.abs}枚#{result[:this_count].to_i<0 ? '減らした':'増やした'}ので、#{result[:user_id]}さんの座布団は#{result[:result_count]}枚になりました！"
       else
         return "@#{from_id}さん！#{result[:user_id]}さんの座布団はもうゼロよ！"
       end
@@ -26,17 +26,18 @@ class ZabutonCount
   # return {[座布団対象id],[座布団の数]}
   #
   def check(status,from_id)
-   param = status.scan(/^@(\w+)\s*(\+\++|--+)/)
+   param = status.scan(/@(\w+)\s*(\+\++|--+)/)
    if param.size != 0
      if param[0].size == 2
        userid = param[0][0]
        countstr = param[0][1]
        if userid != from_id
-         model = save_zabuton( userid, countstr)
+         count = get_count(countstr)
+         model = save_zabuton( userid, count )
          if !model.nil?
-           return { :user_id => model.user_id, :count => model.count , :res => true}
+           return { :user_id => model.user_id,:this_count=>count, :result_count => model.count , :res => true}
          else
-           return { :user_id => userid , :count => 0, :res => false }
+           return { :user_id => userid ,:this_count=>count, :result_count => 0, :res => false }
          end
        end
      end  
@@ -48,9 +49,8 @@ class ZabutonCount
   # DBに値を格納します。
   # 存在しないIDの場合は新規に登録します。
   #
-  def save_zabuton(userid,countstr)
+  def save_zabuton(userid,count)
     model = CountTable.find_by_user_id(userid)
-    count = get_count(countstr)
     if !model.nil?
       if model.count == 0 && count < 0
         return nil
