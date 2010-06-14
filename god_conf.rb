77bf9a62d6b89953a565a4c0da0cb4625aed7091
Hajime WAKAHARA <hajime.wakahara@gmail.com>
2010-06-14 21:51:49 +0900

    refactor god_conf

--- god_conf.rb
+++ god_conf.rb
@@ -1,114 +1,21 @@
-God.pid_file_directory = "#{ENV["HOME"]}/.sakadasan_pids"
-app_root = "#{ENV["HOME"]}/sakadasan"
+project_name = "sakadasan"
+processes = %w|tuple_space pull invent record read update push_adjust adjust|
 
-God.watch do |w|
-  w.name = "tuple_space.rb"
-  w.group = "sakadasan"
-  w.dir = app_root
-  w.start = "#{app_root}/tuple_space.rb"
-  w.behavior(:clean_pid_file)
-  w.start_if do |start|
-    start.condition(:process_running) do |c|
-      c.interval = 3.seconds
-      c.running = false
-    end
-  end
-end
+God.pid_file_directory = "#{ENV["HOME"]}/.#{project_name}_pids"
+app_root = "#{ENV["HOME"]}/#{project_name}"
 
-God.watch do |w|
-  w.name = "pull.rb"
-  w.group = "sakadasan"
-  w.dir = app_root
-  w.start = "#{app_root}/pull.rb"
-  w.behavior(:clean_pid_file)
-  w.start_if do |start|
-    start.condition(:process_running) do |c|
-      c.interval = 3.seconds
-      c.running = false
-    end
-  end
-end
-
-God.watch do |w|
-  w.name = "invent.rb"
-  w.group = "sakadasan"
-  w.dir = app_root
-  w.start = "#{app_root}/invent.rb"
-  w.behavior(:clean_pid_file)
-  w.start_if do |start|
-    start.condition(:process_running) do |c|
-      c.interval = 3.seconds
-      c.running = false
-    end
-  end
-end
-
-God.watch do |w|
-  w.name = "record.rb"
-  w.group = "sakadasan"
-  w.dir = app_root
-  w.start = "#{app_root}/record.rb"
-  w.behavior(:clean_pid_file)
-  w.start_if do |start|
-    start.condition(:process_running) do |c|
-      c.interval = 3.seconds
-      c.running = false
-    end
-  end
-end
-
-God.watch do |w|
-  w.name = "read.rb"
-  w.group = "sakadasan"
-  w.dir = app_root
-  w.start = "#{app_root}/read.rb"
-  w.behavior(:clean_pid_file)
-  w.start_if do |start|
-    start.condition(:process_running) do |c|
-      c.interval = 3.seconds
-      c.running = false
-    end
-  end
-end
-
-God.watch do |w|
-  w.name = "update.rb"
-  w.group = "sakadasan"
-  w.dir = app_root
-  w.start = "#{app_root}/update.rb"
-  w.behavior(:clean_pid_file)
-  w.start_if do |start|
-    start.condition(:process_running) do |c|
-      c.interval = 3.seconds
-      c.running = false
-    end
-  end
-end
-
-God.watch do |w|
-  w.name = "push_adjust.rb"
-  w.group = "sakadasan"
-  w.dir = app_root
-  w.start = "#{app_root}/push_adjust.rb"
-  w.behavior(:clean_pid_file)
-  w.start_if do |start|
-    start.condition(:process_running) do |c|
-      c.interval = 3.seconds
-      c.running = false
-    end
-  end
-end
-
-God.watch do |w|
-  w.name = "adjust.rb"
-  w.group = "sakadasan"
-  w.dir = app_root
-  w.start = "#{app_root}/adjust.rb"
-  w.behavior(:clean_pid_file)
-  w.start_if do |start|
-    start.condition(:process_running) do |c|
-      c.interval = 3.seconds
-      c.running = false
+processes.each do |process|
+  God.watch do |w|
+    w.name = "#{process}.rb"
+    w.group = project_name
+    w.dir = app_root
+    w.start = "#{app_root}/#{process}.rb"
+    w.behavior(:clean_pid_file)
+    w.start_if do |start|
+      start.condition(:process_running) do |c|
+        c.interval = 3.seconds
+        c.running = false
+      end
     end
   end
 end
