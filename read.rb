5334f8b0591c3f820d0a94f785d6812556f3b6ec
Hajime WAKAHARA <hajime.wakahara@gmail.com>
2010-06-30 03:12:46 +0900

    fix the message of the read.rb log to show its own actual username

--- read.rb
+++ read.rb
@@ -12,14 +12,14 @@ log = Logger.new("#{ENV["HOME"]}/log/sakadasan.log")
 log.level = Logger::INFO
 
 while tuple = ts.take([:read, username, nil])
-  log.debug("sakadasan/read.rb got the trigger")
+  log.debug("@#{username}'s read.rb got the trigger")
   begin
     tweet = tuple[2]
     responses = updater.respond(tweet["text"], tweet["user"]["screen_name"])
-    log.debug("Sakadasan responded to some tweet")
+    log.debug("@#{username} responded to some tweet")
     responses.each do |response|
       ts.write([:update, username, response, tweet["id"]])
-      log.info("Sakadasan said \"#{response}\" (in reply to @#{tweet["user"]["screen_name"]})")
+      log.info("@#{username} said \"#{response}\" (in reply to @#{tweet["user"]["screen_name"]})")
     end
   rescue => e
     log.fatal("#{e.class}: #{e.message}")
