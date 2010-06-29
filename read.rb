31398ff800894578d5f60eed16dc9b07121b9b8f
Hajime WAKAHARA <hajime.wakahara@gmail.com>
2010-06-29 12:49:46 +0900

    output the log of read.rb

--- read.rb
+++ read.rb
@@ -2,16 +2,29 @@
 
 require "updater"
 require "drb"
+require "logger"
 
 username = Updater::Username
 ts = DRbObject.new_with_uri(Updater::DRbURI)
 
 updater = Updater.new
+log = Logger.new("#{$HOME}/log/sakadasan.log")
+log.level = Logger::INFO
 
 while tuple = ts.take([:read, username, nil])
-  tweet = tuple[2]
-  responses = updater.respond(tweet["text"], tweet["user"]["screen_name"])
-  responses.each do |response|
-    ts.write([:update, username, response, tweet["id"]])
+  log.debug("sakadasan/read.rb got the trigger")
+  begin
+    tweet = tuple[2]
+    responses = updater.respond(tweet["text"], tweet["user"]["screen_name"])
+    log.debug("Sakadasan responded to some tweet")
+    responses.each do |response|
+      ts.write([:update, username, response, tweet["id"]])
+      log.info("Sakadasan said \"#{response}\" (in reply to @#{tweet["id"]})")
+    end
+  rescue => e
+    log.fatal("#{e.class}: #{e.message}")
+    e.backtrace.each do |info|
+      log.fatal("-- #{info}")
+    end
   end
 end
