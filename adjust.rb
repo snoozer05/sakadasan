4c4a7738b4fb061a93118afc67ad50d882b19ba0
Hajime WAKAHARA <hajime.wakahara@gmail.com>
2010-06-30 02:36:26 +0900

    output the log of adjust.rb

--- adjust.rb
+++ adjust.rb
@@ -6,6 +6,7 @@ require "rubygems"
 require "pit"
 require "oauth"
 require "json"
+require "logger"
 
 username = Updater::Username
 ts = DRbObject.new_with_uri(Updater::DRbURI)
@@ -21,23 +22,35 @@ access_token = OAuth::AccessToken.new(
   user_config[:access_token],
   user_config[:access_token_secret])
 
+logger = Logger.new("#{ENV["HOME"]}/log/#{username}.log")
+logger.level = Logger::INFO
+
 while ts.take([:adjust, username])
   begin
     friends_response = access_token.get("http://api.twitter.com/1/friends/ids.json")
-    friends = JSON.load(friends_response.body)
+    if friends_response.code != "200"
+      next
+    end
     followers_response = access_token.get("http://api.twitter.com/1/followers/ids.json")
+    if followers_response.code != "200"
+      next
+    end
+    friends = JSON.load(friends_response.body)
     followers = JSON.load(followers_response.body)
-  rescue Exception
-    next
-  end
-  ids_to_follow = followers - (friends & followers)
-  ids_to_unfollow = friends - (friends & followers)
-  ids_to_follow.each do |id_to_follow|
-    param = {:user_id => id_to_follow}
-    access_token.post("http://api.twitter.com/1/friendships/create/#{username}.json", param)
-  end
-  ids_to_unfollow.each do |id_to_unfollow|
-    param = {:user_id => id_to_unfollow}
-    access_token.post("http://api.twitter.com/1/friendships/destroy.json", param)
+    ids_to_follow = followers - (friends & followers)
+    ids_to_unfollow = friends - (friends & followers)
+    ids_to_follow.each do |id_to_follow|
+      param = {:user_id => id_to_follow}
+      access_token.post("http://api.twitter.com/1/friendships/create/#{username}.json", param)
+    end
+    ids_to_unfollow.each do |id_to_unfollow|
+      param = {:user_id => id_to_unfollow}
+      access_token.post("http://api.twitter.com/1/friendships/destroy.json", param)
+    end
+  rescue => e
+    logger.fatal("#{e.class}: #{e.message}")
+    e.backtrace.each do |info|
+      logger.fatal("-- #{info}")
+    end
   end
 end
